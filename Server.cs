644cee0f43f07e56536277d3c430ef20155e2202
Lukas <Lukasstancikas@gmail.com>
2014-02-27 09:17:25 +0100

    Test of StartPoint

---
+++ Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPProject
+{
+    public class ClientList
+    {
+        public List<TcpClient> TCPList = new List<TcpClient>();
+        public List<Stream> StreamList = new List<Stream>();
+    }
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(1234);
+            ClientList ServerList = new ClientList();
+            serverSocket.Start();
+            while (true)
+            {
+
+                //Console.WriteLine("Initiated");
+                Service Service = new Service(serverSocket.AcceptTcpClient(), ServerList.TCPList, ServerList.StreamList);
+                Task.Factory.StartNew(() => Service.doIt(ServerList));
+
+
+
+            }
+
+            serverSocket.Stop();
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPProject
+{
+    class Service
+    {
+        private TcpClient _tcpClient;
+        public String Name;
+
+        public TcpClient TCPClient
+        {
+            set { _tcpClient = value; }
+            get { return _tcpClient; }
+        }
+        public Service(TcpClient Client, List<TcpClient> TCPList, List<Stream> StreamList)
+        {
+            TCPList.Add(Client);
+            _tcpClient = Client;
+            StreamList.Add(Client.GetStream());
+
+        }
+
+        public void doIt(ClientList ServerList)
+        {
+            try
+            {
+                Console.WriteLine("Client connected");
+                StreamReader sr = new StreamReader(TCPClient.GetStream());
+                string message = sr.ReadLine();
+                Name = message;
+                string answer = "";
+                while (true)
+                {
+                    message = sr.ReadLine();
+                    Console.WriteLine(Name + " said: " + message);
+                    answer = Name + " said: " + message;
+                    foreach (var VARIABLE in ServerList.StreamList)
+                    {
+                        StreamWriter sw = new StreamWriter(VARIABLE);
+                        sw.AutoFlush = true;
+                        if (!TCPClient.GetStream().Equals(VARIABLE))
+                        {
+                            sw.WriteLine(answer);
+                        }
+                    }
+                }
+
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Caught SocketException");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Caught IOException");
+            }
+        }
+    }
+}
