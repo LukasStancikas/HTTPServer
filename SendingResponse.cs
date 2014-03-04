1a33d9ca92a56f5a798d22c24a6bc31df666faae
Lukas <Lukasstancikas@gmail.com>
2014-03-04 14:35:42 +0100

    fixed streams

--- SendingResponse.cs
+++ SendingResponse.cs
@@ -15,52 +15,44 @@ namespace HTTPProject
 
         public SendingResponse(TcpClient Client, ref String answer, String RootCatalog, ref String requestedFile)
         {
-            StreamWriter writer = new StreamWriter(Client.GetStream());
+            
+            NetworkStream Stream = Client.GetStream();
+            StreamWriter writer = new StreamWriter(Stream);
             FileStream TempStream=null;
-           
+    
             try
             {
 
-               // Here I read from the file in server
+           
                     TempStream = new FileStream(RootCatalog + requestedFile, FileMode.Open);
-                    
-               
+                   
 
             }
             catch (DirectoryNotFoundException e)
             {
                 requestedFile = "/directory_not_found.html";
                 answer = "HTTP/1.0 404 Not Found\r\n";
-                using (FileStream FileReader = new FileStream(RootCatalog + requestedFile, FileMode.Open))
-                {
-
-                    FileReader.CopyTo(TempStream);
-                }
+                TempStream = new FileStream(RootCatalog + requestedFile, FileMode.Open);
+                Console.WriteLine(RootCatalog + requestedFile);
             }
             catch (FileNotFoundException ex)
             {
                 requestedFile = "/page_not_found.html";
                 answer = "HTTP/1.0 404 Not Found\r\n";
-                using (FileStream FileReader = new FileStream(RootCatalog + requestedFile, FileMode.Open))
-                {
-
-                    FileReader.CopyTo(TempStream);
-                }
+                TempStream = new FileStream(RootCatalog + requestedFile, FileMode.Open);
+             
             }
            
            
-            writer.AutoFlush = false;
-            // with first writeline i input 'HTTP/1.0 ... ...\r\nContent-Type: ' and a string Type (it's all in the 'answer')
-            
             writer.WriteLine(answer);
-            Console.WriteLine(answer);
-            //with second writeline i input an empty line to seperate the bodies
-            writer.WriteLine();
             writer.Flush();
-            //now the problem is inputing data i read from the file to the writer, it only accepts Strings, and when using FileStream
-            //I don't get a string from reading the file. That's why I'm figuring out how to send the data of the jpg/html etc..
-            TempStream.CopyTo(Client.GetStream());
-          
+            TempStream.CopyTo(Stream);
+            TempStream.Flush();
+            writer.Close();
+            TempStream.Close();
+            Stream.Close();
+           
+      
             
            
            
